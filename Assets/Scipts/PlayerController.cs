c830dd06acc58759bc9a407b9906199aa2bd8375
Vimarsh Sharma <42619785+Vimu-Sama@users.noreply.github.com>
2022-04-20 16:47:06 +0530

    Boundary and Basic Player Movement Added
    Added basic player movement and player script to bind player in the camera view and have player movement in the same only.

---
+++ Assets/Scipts/PlayerController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 inpuut;
+    [SerializeField] float movespeed=1f;
+    [SerializeField] float top_padding=0f;
+    [SerializeField] float bottom_padding = 0f;
+    [SerializeField] float left_padding = 0f;
+    [SerializeField] float right_padding = 0f;
+
+    Vector2 lower_limit, upper_limit;
+
+    private void Start()
+    {
+        set_bounds();
+    }
+    private void Update()
+    {
+        Vector2 delta = inpuut * Time.deltaTime * movespeed;
+        Vector2 newpos= new Vector2();
+        newpos.x = Mathf.Clamp(transform.position.x+ delta.x, lower_limit.x + left_padding, upper_limit.x - right_padding);
+        newpos.y = Mathf.Clamp(transform.position.y + delta.y, lower_limit.y + bottom_padding, upper_limit.y - top_padding);
+        transform.position = newpos  ;
+    }
+    
+    void set_bounds()
+    {
+        Camera cam = Camera.main;
+        lower_limit = cam.ViewportToWorldPoint(new Vector2(0,0));
+        upper_limit = cam.ViewportToWorldPoint(new Vector2(1, 1));
+
+    }
+
+    void OnMove(InputValue value)
+    {
+        inpuut= value.Get<Vector2>();
+    }
+}
